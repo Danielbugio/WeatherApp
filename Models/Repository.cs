e7321de8f2f22276f65e5285181430be74638a3b
Daniel Domingues <danielbugio@hotmail.com>
2022-10-05 18:21:43 +0100

    1

---
+++ Models/Repository.cs
@@ -0,0 +1,50 @@
+﻿namespace WeatherApp.Models
+{
+    public static class Repository
+    {
+        public static List<Weather> Weathers
+        {
+            get
+            {
+                WeatherDbContext context = new WeatherDbContext();
+                List<Weather> weathers = context.Weathers.ToList();
+                return weathers;
+            }
+        }
+
+        public static void AddWeather(Weather newWeather)
+        {
+            WeatherDbContext context = new WeatherDbContext();
+            context.Weathers.Add(newWeather);
+            context.SaveChanges();
+        }
+
+        public static void DeleteWeather(int weatherID)
+        {
+            WeatherDbContext context = new WeatherDbContext();
+            List<Weather> weathers = context.Weathers.ToList();
+
+            for (int i = 0; i < Weathers.Count; i++)
+            {
+                if (weatherID == weathers[i].WeatherID)
+                {
+                    context.Weathers.RemoveRange(weathers[i]);
+                }
+            }
+            context.SaveChanges();
+        }
+
+        public static Weather AlterarWeather(Weather myWeather)
+        {
+            WeatherDbContext context = new WeatherDbContext();
+
+            context.Weathers.Update(myWeather);
+
+            context.SaveChanges();
+
+            return myWeather;
+        }
+
+
+    }
+}
---
+++ Models/WeatherDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeatherApp.Models
+{
+    public class WeatherDbContext: DbContext
+    {
+        public DbSet<Weather> Weathers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection =
+            @"Server=(localdb)\mssqllocaldb;Database=WeatherApp;
+        Trusted_Connection=True;";
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(connection);
+        }
+    }
+
+   
+}
