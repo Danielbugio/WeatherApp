89248d53d98daff3429d42430644370a0858804f
Daniel Domingues <danielbugio@hotmail.com>
2022-10-05 21:32:54 +0100

    OnConfiguring error

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherApp.Models;
+
+namespace WeatherApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Weather> Weathers { get; set; }
+    }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connection = @"Server=(localdb)\mssqllocaldb;Database=WeatherApp; Trusted_Connection=True;";
+        optionsBuilder.UseLazyLoadingProxies().UseSqlServer(connection);
+    }
+
+
+}
