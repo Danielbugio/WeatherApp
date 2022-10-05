317cea0d81d8bda030a2904d8b66c9b85f0f9dbe
Daniel Domingues <danielbugio@hotmail.com>
2022-10-05 17:48:23 +0100

    Add project files.

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult InsertWeather()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult InsertWeather(Weather newWeather)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("WeatherAdded", newWeather);
+            }
+            else 
+                return View();
+        }
+    }
+}
---
+++ Models/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+
+        public int WeatherID { get; set; }
+
+        public string? City { get; set; }
+
+
+        [Required(ErrorMessage = "Please insert temperature")]
+        public double Temperature { get; set; }
+
+        [Required(ErrorMessage = "Please insert Date")]
+        [Column(TypeName = "Date")]
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public string Date { get; set; }
+
+    }
+}
